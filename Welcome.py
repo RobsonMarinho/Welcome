e759af4c0a80a4f8e1afbb4c7f34f6c146a623d4
Robson Marinho <robson_luz15@hotmail.com>
2019-05-09 21:33:39 -0300

    Finalizando retorno de 'Boas vindas'

--- Welcome.py
+++ Welcome.py
@@ -1,11 +1,24 @@
-import pyttax3
+import pyttsx3
 
 from datetime import datetime
 
+#
 agora = datetime.now()
+
 # Contém nome do usuário
 nome = "Robson"
+
 # Exibe a hora atual
 hora = agora.hour
+
 # Exibe o minuto atual
 minuto = agora.minute
+
+texto = "Olá " + str(nome) + " seja bem vindo! Agora são " + str(hora) + " horas e " + str(minuto) + " minutos."
+
+en = pyttsx3.init()
+en.setProperty('rate', 125) # propriedade de velocidade
+en.setProperty('volume', 1) # propriedade de volume
+en.setProperty('voice', b'brazil')# propriedade de idioma
+en.say(texto)# propriedade de texto falado
+en.runAndWait()
