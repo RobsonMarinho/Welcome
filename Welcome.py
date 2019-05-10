fe3a495e4e41aa81a0bcd29748d004514dfcd47a
Robson Marinho <robson_luz15@hotmail.com>
2019-05-09 21:16:20 -0300

    Subindo primeiros arquivos e códigos do projeto

---
+++ Welcome.py
@@ -0,0 +1,11 @@
+import pyttax3
+
+from datetime import datetime
+
+agora = datetime.now()
+# Contém nome do usuário
+nome = "Robson"
+# Exibe a hora atual
+hora = agora.hour
+# Exibe o minuto atual
+minuto = agora.minute
